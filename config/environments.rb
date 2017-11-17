ceca5e113cbf7933edccb52fc75c2d6830c4a42b
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-11-17 10:39:19 -0600

    corrected environments.rb

--- config/environments.rb
+++ config/environments.rb
@@ -2,7 +2,7 @@ configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/weather')
 
 	ActiveRecord::Base.establish_connection(
-		:adapter  => db.schema == 'postgres' ? 'postgresql' : db.schema,
+		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
 		:host     => db.host,
 		:username => db.user,
 		:password => db.password,
